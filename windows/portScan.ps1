936d3f2fd3c39f14eeb436999075744dc301400f
kevin Lyon <blackmaskdeveloper@gmail.com>
2023-03-06 13:56:59 -0300

    reeorder and add windows script

---
+++ windows/portScan.ps1
@@ -0,0 +1,16 @@
+﻿param($target)
+if(!$target){
+    echo "Por favor digite um alvo válido"
+    echo "exemplo \.script.ps1 google.com"
+} else { 
+    foreach($port in 50..80){
+        $replyt = Test-netconnection $target -P $port -WarningAction SilentlyContinue -InformationLevel Quiet
+       #echo $replyt
+        if($replyt -eq "True"){
+            echo "${target}:${port} -- OPEN" 
+        } else {
+            #Porta fechada
+            }
+    }
+    #Test-netconnection $target -P 80 -WarningAction SilentlyContinue -InformationLevel Quiet
+}
